2d3d297ffb10e34073f63359960b2825c1c47c4b
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2020-04-06 11:31:05 +0530

    added -b for n4biasfield

--- secondN4.sh
+++ secondN4.sh
@@ -1,4 +1,4 @@
-N4BiasFieldCorrection -d 3 -i site-mcgill_sub-032208_ses-001.nii.gz  -o [output_image.nii.gz,bias_image.nii.gz]
+N4BiasFieldCorrection -d 3 -b [100] -i site-mcgill_sub-032208_ses-001.nii.gz  -o [output_image.nii.gz,bias_image.nii.gz]
 3dSkullStrip -input output_image.nii.gz -surface_coil -prefix in_file_b.nii.gz -monkey
 3dcalc -a output_image.nii.gz -b in_file_b.nii.gz -expr 'a*step(b)' -prefix out_file.nii.gz
 fslmaths out_file.nii.gz -thr 0.001 out_mask.nii.gz
