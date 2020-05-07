8a9157e5e5e8d102cda668c58c671a355a07f428
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2020-05-07 11:57:30 +0530

    Update for compatibility

--- secondN4.sh
+++ secondN4.sh
@@ -1,8 +1,18 @@
-N4BiasFieldCorrection -d 3 -b [100] -i input.nii.gz  -o [output_image.nii.gz,bias_image.nii.gz]
-#3dSkullStrip -input output_image.nii.gz -surface_coil -prefix in_file_b.nii.gz -monkey
-#3dcalc -a output_image.nii.gz -b in_file_b.nii.gz -expr 'a*step(b)' -prefix out_file.nii.gz
-#fslmaths out_file.nii.gz -thr 0.001 out_mask.nii.gz
-#Original mask file has datatype = 512 which is leading to errors
-fslmaths /misc/evarts2/MONKEY_MRIQC/NEW_EXAMPLES_RUN/mask/site-inb_sub-032401_ses-001_mask.nii.gz -thr 0.0001 out_mask.nii.gz
-mv output_image.nii.gz bias_corrected.nii.gz
-fslmaths bias_corrected.nii.gz -mul out_mask.nii.gz out_file.nii.gz
+#!/bin/bash
+
+#kilimanjaro2
+#7 May 2019
+
+bidsdir="/home/kilimanjaro2/Research/monkeyStuff/bidsData"
+
+for inp in ./sub-*; do
+  tmp=`basename ${inp}`
+  echo ${tmp}
+  cd "${tmp}/"
+  N4BiasFieldCorrection -d 3 -b [100] -i ${bidsdir}/${tmp}/anat/${tmp}_T1w.nii.gz  -o [bias_corrected.nii.gz,bias_image.nii.gz]
+  fslmaths out_mask.nii.gz  -thr 0.0001 out_tmp_mask.nii.gz
+  rm out_mask.nii.gz
+  mv out_tmp_mask.nii.gz out_mask.nii.gz
+  fslmaths bias_corrected.nii.gz -mul out_mask.nii.gz out_file.nii.gz
+cd -
+done;
